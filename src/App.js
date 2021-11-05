c89608611b3daab4814ccecd9ac11aaf06275f64
Andrey Kruglik <isekaiguy@protonmail.com>
2021-11-06 02:45:34 +0700

    Final update to Firebase v9

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
 
-import './App.css';
+import HomePage from "./pages/homepage/homepage.component";
+import Shop from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import { userAuth, createUserProfileDocument } from "./firebase/firebase.utils";
 
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import Header from './components/header/header.component';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import "./App.scss";
 
-class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null
-    };
-  }
+class App extends Component {
+  state = {
+    currentUser: null,
+  };
 
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+    this.unsubscribeFromAuth = userAuth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
-        const userRef = await createUserProfileDocument(userAuth);
+        const { userRef, onSnapshot } = await createUserProfileDocument(
+          userAuth
+        );
 
-        userRef.onSnapshot(snapShot => {
+        onSnapshot(userRef, (snapshot) => {
           this.setState({
             currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
+              id: snapshot.id,
+              ...snapshot.data(),
+            },
           });
-        });
-      }
 
-      this.setState({ currentUser: userAuth });
+          console.log(this.state);
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
     });
   }
 
@@ -48,9 +48,9 @@ class App extends React.Component {
       <div>
         <Header currentUser={this.state.currentUser} />
         <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={Shop} />
+          <Route path="/signin" component={SignInAndSignUpPage} />
         </Switch>
       </div>
     );
